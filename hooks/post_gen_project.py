dc54416dbe4b0d8428e1f42eee049d7b4ac70e88
Steven van de Graaf <steven@vandegraaf.xyz>
2023-10-17 21:21:33 +0200

    :recycle: Add, update and run pre-commit hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,19 +4,35 @@ import sys
 
 # perform git initialization
 if "{{ cookiecutter.init_git }}" == "True":
-    subprocess.run(["git", "init", "-b", "main"])
-    subprocess.run(["git", "add", "."])
-    subprocess.run(["git", "commit", "-m", "🍪 Initial commit from `cookiecutter-python-package`"])
+    subprocess.run(["git", "init", "-b", "main"], check=False)
+    subprocess.run(["git", "add", "."], check=False)
+    subprocess.run(
+        ["git", "commit", "-m", "🍪 Initial commit from `cookiecutter-python-package`"],
+        check=False,
+    )
 
 # create venv and install dependencies
 if "{{ cookiecutter.init_venv }}" == "True":
-    subprocess.run([sys.executable, "-m", "venv", ".venv"])
+    subprocess.run([sys.executable, "-m", "venv", ".venv"], check=False)
     if os.name == "posix":
         venv_executable = r".venv/bin/python"
     else:
         venv_executable = r".venv\Scripts\python"
-    subprocess.run([venv_executable, "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel", "flit"])
+    subprocess.run(
+        [
+            venv_executable,
+            "-m",
+            "pip",
+            "install",
+            "--upgrade",
+            "pip",
+            "setuptools",
+            "wheel",
+            "flit",
+        ],
+        check=False,
+    )
     # install dependencies
-    subprocess.run([venv_executable, "-m", "flit", "install", "--symlink"])
+    subprocess.run([venv_executable, "-m", "flit", "install", "--symlink"], check=False)
     # install pre-commit hooks
-    subprocess.run(["pre-commit", "install"])
+    subprocess.run(["pre-commit", "install"], check=False)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -5,4 +5,5 @@ PACKAGE_PATTERN = re.compile(r"^[_a-zA-Z][_a-zA-Z0-9]+$")
 package_name = "{{ cookiecutter.package_name }}"
 
 if PACKAGE_PATTERN.match(package_name) is None:
-    raise ValueError(f"Invalid package name: `{package_name}`. Please only use alphanumerics and underscores (_).")
+    msg = f"Invalid package name: `{package_name}`. Please only use alphanumerics and underscores (_)."
+    raise ValueError(msg)
